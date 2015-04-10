7dbe9bc09caffeef5d6a2e40d740ad3bf3cb0cf2
rbalchand <you@example.com>
2015-04-10 11:08:14 -0400

    Kiosk

--- JAVA/Tree.java
+++ JAVA/Tree.java
@@ -12,6 +12,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Tree{
     public TreeNode root;
@@ -74,7 +75,10 @@ public class Tree{
 	return ans;
     }
        
-	 
+    /**
+     *method that prints all of the nodes in a current tree using pre order traversal
+     *@param focusNode the starting node, usually the root
+     */	 
     public void printAll(TreeNode focusNode){
 	if(focusNode != null){
 	    System.out.println(focusNode);
@@ -151,7 +155,10 @@ public class Tree{
     }	
 	
 
-
+    /**
+     *reads data from a file and storees in the tree accordingly
+     *@param file the name of the file used
+     */
     public void readFile(String file){
 	BufferedReader br = null;
 	try{
@@ -183,7 +190,7 @@ public class Tree{
 			    String childSelection = br.readLine();
 			    String childMessage = br.readLine();
 			    String newNodeName = nodeName.replaceAll(" ", "");
-			    System.out.println(this.addNode(childName, childSelection, childMessage, newNodeName));
+			    this.addNode(childName, childSelection, childMessage, newNodeName);
 			}
 		   }
 	    }
@@ -214,11 +221,11 @@ public class Tree{
 	if(this.getRoot() == null){
 	    ans = false;
 	}
+	else if(this.findNode(parentName) == null){
+	    System.out.println("input file is invalid");
+	}
 	else{
 	    TreeNode parent = this.findNode(parentName);
-	    if(parent == null) {
-		System.out.println("this bne he problem");
-	    }
 	    String dest = name.substring(name.length() - 1, name.length());
 	    if(dest.equals("1")){
 		parent.setLeft(adding);
@@ -236,77 +243,161 @@ public class Tree{
 	return ans;
     }
 
+
+
+    /**
+     *initiates ordering service; starting from the root of the tree and prompts users for inputs; choose the branch of the tree that the user is interested in. Print the order and the price when a leaf is reached.
+     */
+    public void beginSession(){
+	boolean chosen = true;
+	Scanner scanner = new Scanner(System.in);
+	String ans = "";
+	System.out.println("Help Session Starting ...");
+	TreeNode focusNode = this.getRoot();
+	int counter = 0;
+	while(focusNode.isLeaf() == false){
+	    System.out.println("\n" + focusNode.getMessage());
+	    int numItem = 1;
+	    if(focusNode.getLeft() != null){
+		System.out.println(numItem  + " " +focusNode.getLeft().getSelection());
+		numItem += 1;
+	    }
+	    if(focusNode.getMiddle() != null){
+		System.out.println(numItem + " " + focusNode.getMiddle().getSelection());
+		numItem += 1;
+	    }
+	    if(focusNode.getRight() != null){
+		System.out.println(numItem + " " + focusNode.getRight().getSelection());
+		numItem += 1;
+	    }
+	    System.out.println("0 Exit Session");
+	    int choice = -1;
+	    try{
+		while(choice < 0 || choice > numItem){
+		    
+		    System.out.print("Choice: ");
+		    choice = scanner.nextInt();
+		    if(choice < 0 || choice > numItem){
+			System.out.println("Not in range");
+		    }
+		}
+	    }catch(Exception exception){
+		System.out.println("Please enter an integer choice: ");
+	    }
+	
+
+	    if(choice == 0){
+		chosen = false;
+		System.out.println("\nExiting Session...");
+		break;
+	    }else{
+		counter++;
+	    
+		if(focusNode.getName() != "root"){
+		    ans += focusNode.getSelection() + "mark";
+		}
+		
+		if((choice == 1) && (choice <= numItem)){
+		    focusNode = focusNode.getLeft();
+		}
+		if((choice == 2) && (choice <= numItem)){
+		    focusNode = focusNode.getMiddle();
+		}
+		if((choice == 3) && (choice <= numItem)){
+		    focusNode = focusNode.getRight();
+		}
+	    }
+	}
+	if(chosen == true){
+	    ans += focusNode.getSelection() + "mark";
+	    ans += focusNode.getMessage();
+	    String [] answers = ans.split("mark");
+	    String summary = "The order at " + answers[0] + ": ";
+	    String options = "";
+	    for(int i = 1; i < answers.length - 1; i++){
+		options += answers[i];
+		if(i != answers.length - 2){
+		    options += ", ";
+		}
+		if(i == answers.length - 2){
+		    options += " ";
+		}
+	    }
+	    options += " has been sent to the kitchen. ";
+	    String price = "Total amount would be " + answers[answers.length - 1] + ". ";
+	    String finish = summary + options + price;	
+	    
+	    System.out.println("\n" + finish);    
+	}
+    }
     /**
      * prints the combination of the whole selection menu; should include item selection, and price
      *@param parentInfo the information about the parent that is used to print from
      */
-    /* public void printMenu(String parentInfo){
-       if(parentInfo == "root"){
-       System.out.println("Menu: ");
-	    System.out.printf("Dining" + "%10s" + "Selection" + "%50s" + "Price \n", " ", " ");
-	    System.out.println("-----------------------------------------------------------------------------------\n");
-	}
-
-	TreeNode focusNode = new TreeNode();
-	focusNode = this.findNode(parentInfo);
-	if(focusNode != null){
-	    if(focusNode.isLeaf()){
-		System.out.printf("%-s" + "\n", focusNode.getMessage());
-	    }else{
-		if(focusNode//
-		
-    
-		}*/
-
-
-    
-
-
-
-
-
-
-
-
-    
-    public static void main(String [] args){
-	Tree a = new Tree();
-	/*TreeNode root = new TreeNode("root","0", "root", null, null, null);
-	a.setRoot(root);
-	TreeNode left = new TreeNode("left", "1", "left", null, null, null);
-	TreeNode middle = new TreeNode("middle", "2", "middle", null, null, null);
-	TreeNode right = new TreeNode("right", "3", "right", null, null, null);
-	TreeNode leftleft = new TreeNode("left2", "4", "left", null, null, null);
-	left.setLeft(leftleft);
-	root.setLeft(left);
-	root.setRight(right);
-	root.setMiddle(middle);
 	
-	//	System.out.println(a.findNode("left").getName());
-	TreeNode treeRoot = a.getRoot();
-	//	System.out.println(a.findNode("right"));
-	//a.printAll(treeRoot);
-	//	a.setRoot(left);
-	//	TreeNode[] array = new TreeNode[0];
-	TreeNode[] array2 = a.toArray();
-	for(int i = 0; i < array2.length; i++){
-	    System.out.println(array2[i] + " ");
+	
+    public void printMenu(String parentInfo){
+	if(parentInfo == "root"){
+	    System.out.println("Menu: ");
+	    System.out.printf("Dining" + "%10s" + "Selection" + "%50s" + "Price \n", " ", " ");
+	    System.out.println("-----------------------------------------------------------------------------------\n"); 
 	}
-	System.out.println("\n\n\n" + a.findNode("middle4"));
-	//TreeNode middlemiddle = new TreeNode("middle", "5", "middle", null, null, null);
-	System.out.println(a.addNode("middle2", "sdvcdsvc2", "middle2", "middle"));
-	a.printAll(root);
-
-	//a.printMenu("root");
-	*/
-	a.readFile("sample.txt");
-	TreeNode band = a.getRoot();
-	a.printAll(band);
-	//	String a1 = "root 3";
-	//String a2 = a1.substring(0, a1.length() - 2);
-	//System.out.println(a2);
-	//System.out.println(Integer.parseInt(a1.substring(a1.length() - 1)));
+	TreeNode focusNode = this.findNode(parentInfo);
+	if(focusNode.getLeft() != null){
+	    String header = focusNode.getLeft().getSelection();
+	    String ans = "";
+	    printMenuHelper(header, focusNode, focusNode.getLeft(), ans);
+	}
+	if(focusNode.getMiddle() != null){
+	    String header1 = focusNode.getMiddle().getSelection();
+	    String ans1 = "";
+	    printMenuHelper(header1, focusNode, focusNode.getMiddle(), ans1);
+	}
+	if(focusNode.getRight() != null){
+	    String header2 = focusNode.getRight().getSelection();
+	    String ans2 = "";
+	    printMenuHelper(header2, focusNode, focusNode.getRight(), ans2);
+	}
+	System.out.println("\n");
+	
     }
+
+
+    /**
+     *The helper method for the print menu method
+     *@param header the first node of the location
+     *@param parent the parent node to keep track of the node
+     *@param current the main search node
+     *@param printOut the thing that is being printed out
+     */
+    public void printMenuHelper(String header, TreeNode parent, TreeNode current, String printOut){
+	if(current.isLeaf() == true){
+	    String finalSelect = current.getSelection();
+	    String price = current.getMessage();
+	    System.out.printf("%-16s"+ "%-1s" + "%-1s" +  "%-10s" , header, printOut, ", ", finalSelect);
+	    System.out.printf("%35s" + "\n", price);
+	}
+	
+	else{
+	    String currentSelect = current.getSelection();
+	    if(current.getSelection().equals(header)){
+		currentSelect = "";
+	    }
+	    if(!(currentSelect.equals(""))){
+		    printOut += ", " + currentSelect;
+		
+	    }
+	    if(current.getLeft() != null){
+		printMenuHelper(header, current, current.getLeft(), printOut);
+	    }
+	    if(current.getMiddle() != null){
+		printMenuHelper(header, current, current.getMiddle(), printOut);
+	    }
+	    if(current.getRight() != null){
+		printMenuHelper(header, current, current.getRight(), printOut);
+	    }
+	}
+    }   
 	    
-    
-}
+}		      
+		      
