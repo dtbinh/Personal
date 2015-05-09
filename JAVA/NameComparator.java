cb28a7b054c8384e5820ebf31132930044c08e8b
rbalchand <you@example.com>
2015-05-09 17:17:16 -0400

    add

--- JAVA/NameComparator.java
+++ JAVA/NameComparator.java
@@ -1,13 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+import java.util.Comparator;
 
 /**
- *
- * @author Ronald
+ *Ronald Balchand
+ *109806273
+ *Assignment #7
+ *CSE 214 Recitation Section 05
+ *Recitation TA: Vyassa Baratham
+ *Grading TA: Ke Ma
+ *@author Ronald Balchand
  */
-public class NameComparator {
+
+
+
+public class NameComparator implements Comparator<City>{
+
+    public int compare(City o1, City o2){
+        int ans = 0;
+        City a = (City)o1;
+        City b = (City)o2;
+        ans = (a.getName().compareTo(b.getName()));
+        return ans;
+    }
     
 }
--- JAVA/SigmaAir.java
+++ JAVA/SigmaAir.java
@@ -9,12 +9,27 @@
  *@author Ronald Balchand
  */
 
+import java.util.*;
 import latlng.LatLng;
 import java.util.ArrayList;
 import com.google.code.geocoder.*;
 import com.google.code.geocoder.model.*;
+import java.util.Comparator;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.Collections;
+import java.io.IOException;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 
-public class SigmaAir{
+
+
+public class SigmaAir implements Serializable{
     private ArrayList<City> cities;
     public static final int MAX_CITIES = 100;
     private double[][] connections;
@@ -49,11 +64,11 @@ public class SigmaAir{
     
     public void addCity(String city){
          String name = city;
-        if(this.exists(city)){
-            System.out.println("This city already is in the index");
-        }
-        else{
-            try{
+        try{
+            if(this.exists(city)){
+                System.out.println("This city already is in the index");
+            }
+            else{
                 Geocoder geocoder = new Geocoder();
                 GeocoderRequest geocoderRequest;
                 GeocodeResponse geocodeResponse;
@@ -73,12 +88,20 @@ public class SigmaAir{
                 this.cities.add(newCity);
             
                 System.out.println(name + " has been added: (" + lat + ", " + lng +  ")");
-            
-            }catch(Exception e){
-                System.out.println("There was a problem");
             }
+            
+        }catch(Exception e){
+            System.out.println("There was a problem");
         }
+        
     }
+    
+    
+    
+    
+    
+    
+    
     
     /**
      * if appropriate cities are given, calculate the distance between the cities, and include this entry in our adjacency table
@@ -104,8 +127,11 @@ public class SigmaAir{
         
             boolean fromfound = false;
             boolean tofound = false;
+            
             int fromInt = 0;
             int toInt = 0;
+            
+            
             for(int i = 0; i < this.cities.size(); i++){
                 City temp = this.cities.get(i);
                 String tempName = temp.getName();
@@ -135,10 +161,8 @@ public class SigmaAir{
             }
     
             else{
-                System.out.println("Wrong");
+                //System.out.println("Wrong");
             }
-        
-        
         
         }catch(Exception ex){
             System.out.println("Error");
@@ -149,19 +173,21 @@ public class SigmaAir{
         
     public boolean exists(String city){
         boolean ans = false;
-        for(int i = 0; 0 < this.cities.size(); i++){
+        int i = 0; 
+        while(i < this.cities.size()){
             City temp = this.cities.get(i);
             String tempName = temp.getName();
             if(city.equals(tempName)){
                 ans = true;
             }
+            i++;
         }
         return ans;
     }
     
     
     /**
-     * if appropriate cities are given, remove the entry in our adjacency table (set value to ∞)
+     * if appropriate cities are given, remove the entry in our adjacency table (set value to âˆž)
      * @param cityFrom the source city in the connection
      * @param cityTo the destination city in the connection
      */
@@ -197,6 +223,8 @@ public class SigmaAir{
                     toInt = temp.getIndexPos();  
                 }
             }
+            
+            
             if((fromfound == false) || (tofound == false)){
                 System.out.println("This connection doesnt exist!");
             }
@@ -243,26 +271,127 @@ public class SigmaAir{
         return ans;
     }
     
+    /**
+     * displays another menu that allows the user to sort the list based on a specific field.
+     * @param comp determines which way its being sorted
+     */
     
-    
-    public void printAllCities(Comprator comp){
+    public void printAllCities(String comp){
+        System.out.println("Cities:");
+        System.out.println("City Name                   Latitude        Longitude");
+        System.out.println("-------------------------------------------------------------");
+        
+        ArrayList<City> temp;
+        temp = this.cities;
+        
+        if(comp.equals("a")){
+            Collections.sort(temp, new NameComparator());
+        }
+        if(comp.equals("b")){
+            Collections.sort(temp, new LatComparator());
+        }
+        if(comp.equals("c")){
+            Collections.sort(temp, new LngComparator());
+        }
+        
+        for(City city: temp){
+            LatLng ll = city.getLocation();
+            double d1 = ll.getLat();
+            double d2 = ll.getLng();
+            System.out.printf("%-25s" + "%14s" + "%15s" + "\n", city.getName(),d1, d2);
+            
+        }
         
     }
     
+    /**
+     * prints all connections available from all recorded cities 
+     */
     
-    
-    
-    
-    
-    
-    
-    public static void main(String [] args){
-        SigmaAir a = new SigmaAir();
-        a.addCity("New York");
-        a.addCity("Toyko");
-        a.addConnection("New York", "Tokyo");
-        a.removeConnection("Paris", "Tokyo");
+    public void printAllConnections(){
+        System.out.println("Connections");
+        System.out.println("Route                               Distance");
+        System.out.println("----------------------------------------------------------");
+        for(int i = 0; i < 100; i++){
+            for(int j = 0; j < 100; j++){
+                if(!(this.connections[i][j] == Double.POSITIVE_INFINITY)){
+                    City src4 = this.cities.get(i);
+                    City dest4 = this.cities.get(j);
+                    String name1 = src4.getName();
+                    String name2 = dest4.getName();
+                    double distance = this.connections[i][j];
+                    System.out.println(name1 + "  -->  " + name2 + "     " + distance);
+                }
+            }
+        }
+        
     }
     
+    /**
+     * parses a file and add all cities to the city list. Assume file content is correct.
+     * @param filename the name of the file being parsed
+     */
+    public void loadAllCities(String filename)throws Exception{
+        String pathname;
+        pathname = "./" + filename;
+        File notYet = new File(pathname);
+        if(notYet.exists() == false){
+            try{
+                throw new Exception();
+            }catch (Exception ex){
+                System.out.println("the file you are looking to draw from does not exist");
+            }
+        }
+        else{
+            BufferedReader br = null;
+            try{
+                String currentLine;
+                br = new BufferedReader(new FileReader(pathname));
+                while((currentLine = br.readLine()) != null){
+                    String name = currentLine;
+                    this.addCity(name);
+                }
+            }catch (Exception ex){
+                System.out.println("problem reading the file");
+            }
+            
+        } 
+    }
     
+    /**
+     * parses a file and add all connections to the adjacency matrix
+     * @param filename 
+     */
+    public void loadAllConnections(String filename)throws Exception{
+        String pathname;
+        pathname = "./" + filename;
+        File notYet = new File(pathname);
+        if(notYet.exists() == false){
+            try{
+                throw new Exception();
+            }catch (Exception ex){
+                System.out.println("the file you are looking to draw from does not exist");
+            }
+        }
+        else{
+            BufferedReader br = null;
+            try{
+                String currentLine;
+                br = new BufferedReader(new FileReader(pathname));
+                while((currentLine = br.readLine()) != null){
+                    String line = currentLine;
+                    String[] destinations = line.split(",");
+                    String src3 = destinations[0];
+                    String dest3 = destinations[1];
+                            
+                    this.addConnection(src3, dest3);
+                }
+            }catch(Exception ex){
+                
+            }
+                
+        }
+        
+    }
+   
 }
