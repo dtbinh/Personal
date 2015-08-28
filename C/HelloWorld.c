beabc80cf881c59c224e654feb18d492892c71b9
rbalchand <you@example.com>
2015-08-28 10:02:40 -0400

    starting cse 130

--- C/HelloWorld.c
+++ C/HelloWorld.c
@@ -2,5 +2,5 @@
 #include <stdio.h>
 
 int main(){
-  printf("HelloWorld");
+  printf("HelloWorld...\n");
 }
---
+++ C/input.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main(void){
+  int number;
+  printf("How many coins are we counting?");
+  scanf("%d",&number);
+ 
+  return 0;
+  
+}
+  
