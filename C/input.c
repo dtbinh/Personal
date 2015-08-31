cc124a42357938f7650f4df452a3c36979169650
rbalchand <you@example.com>
2015-08-31 10:40:29 -0400

    continue

--- C/input.c
+++ C/input.c
@@ -3,9 +3,30 @@
 
 int main(void){
   int number;
-  printf("How many coins are we counting?");
-  scanf("%d",&number);
- 
+  //printf("How many coins are we counting?");
+  //scanf("%d",&number);
+
+
+  printf("Get Set: %s %d %f %c%c", "one", 2, 3.33, 'G', 'O');
+  /*
+    %s == string
+    %d == integer
+    %f == float
+    %c == character
+    %e scientific notation
+  */
+
+
+  //scanf conversion characters
+  /*
+    %c character
+    %d decimal integer
+    %f floating point number
+    %lf long floating point number
+    %s string
+  */
+
+  //character consist of lowercase and uppercase digits and other characters
   return 0;
   
 }
