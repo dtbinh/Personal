4f3546a7a23505e2babfc1fdeff134e830c3ba16
rbalchand <you@example.com>
2015-09-02 14:58:17 -0400

    lab 2

---
+++ C/RONALDBALCHANDlab2.c
@@ -0,0 +1,15 @@
+//RONALD BALCHAND
+//109806273
+//CSE 130, LAB 2
+//LAB 2 SAMPLE PROGRAM
+//learn how to program in c
+
+#include <stdio.h>
+
+int main(void){//this is the main program
+  printf("These lines are printed by \n");
+  printf("the lab2.c program \n");
+
+  return 0;
+}
+   
