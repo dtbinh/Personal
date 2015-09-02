7dffe946c24c3f3b86114ea1b5b3d7a0c30c2864
rbalchand <you@example.com>
2015-09-02 15:13:05 -0400

    lab 2 finished

--- C/RONALDBALCHANDlab2.c
+++ C/RONALDBALCHANDlab2.c
@@ -7,8 +7,27 @@
 #include <stdio.h>
 
 int main(void){//this is the main program
+  
   printf("These lines are printed by \n");
   printf("the lab2.c program \n");
+
+  int coins = 0;
+  printf("How many coins do you have? ");
+  scanf("%d", &coins);
+  printf("You have %d coins\n", coins);
+
+  int n = 0;
+  printf("Enter a number n:  ");
+  scanf("%d", &n);
+
+  int i = 0;
+  int sum = 0;
+  while(i <= n){
+    sum = sum + i;
+    i = i + 1;
+  }
+
+  printf("\nThe sum of all numbers from 1 to %d is %d", n, sum);
 
   return 0;
 }
