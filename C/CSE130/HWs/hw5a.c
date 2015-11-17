3dec261f96b3ecc9984290a8703e0d69500e505a
U-RONALD\Ronald <ronaldbalchand@gmail.com>
2015-11-17 00:54:24 -0500

    Progress

--- C/CSE130/HWs/hw5a.c
+++ C/CSE130/HWs/hw5a.c
@@ -18,45 +18,95 @@ int counter(int * a, int b, int n){//program returns how many occurrences of b t
 }
 
 
+
+int * sort(int offset, int lower, int higher, int *a){//made for integer arays ranging 1-100
+  int count[100];
+  int i3;
+  for(i3 = 0; i3 < 100; i3++){
+    count[i3] = 0;
+  }
+  int i;
+  for(i = lower; i <= higher; i++){//go through the lower to upper bound
+    if(i != 0){
+      if(counter(a, i, 100) != 0){//if there are more than 0 of this number in the array entered
+	count[i + offset - 1] = counter(a, i, 100);// its respective place in the count array is equal to the number of it in the entered array
+      }
+    }
+    
+  }
+
+  int i2;
+  for(i2 = 0; i2 < 100; i2++){
+    printf("%d, ", count[i2]);
+  }
+  
+  return a;
+}
+
+
 int main(null){
-  printf("Enter the range of the numbers you wish to sort: ");
-  int range;
-  scanf("%d", &range);
-  
-  int count[range];
-  
+  int first;
+  int last;
+  printf("Would you like to enter your own offset value(y/n)");
   char ans;
-  printf("Would you like to use default start (1) or input your own offset value(y/n)? ");
-  int start;
-  int finish;
-  
-  scanf(" %c", &ans);
-  fflush(stdin);
-  if(ans == 'y'){//user wants an output
-    int offset;
-    printf("\nEnter your own offset value: ");
-    scanf(" %d", &offset);
-    fflush(stdin);
-    start = (1 - offset);
+  int offset;
+  scanf("%c%*c", &ans);
+  if(ans == 'y'){
+    printf("\nEnter your offset value: ");
+    scanf(" %d%*c",&offset);
+    first = 1 - offset;
+    last = first + 99;
+    printf("\nSorting from %d to %d .\n", first, last);
   }else{
-    start = 1;
+    offset = 0;
+    first = 1;
+    last = 100;
+  }
+  printf("\nNow enter the numbers that you would like to sort : ");
+  int count[100];
+  int numbers [100];
+  int i3 = 0;
+  while(i3 < 100){
+    numbers[i3] = 0;
+    i3++;
+  }
+  
+  char line[1024];
+
+  char *a;
+  char *b;
+  int v;
+  int i2 = 0;
+
+  while(fgets(line, sizeof(line), stdin)){
+    a = line;
+    for(a = line; ; a = b){
+      v = strtol(a, &b, 10);
+      if(a == b){
+	break;
+      }
+      numbers[i2] = v;
+      i2++;
+    }
+    break;
   }
 
-  finish = start + range - 1; 
-  printf("\nYour range will be from %d - %d", start, finish);
 
+  printf("\n");
+  
   int i1;
-  for(i1 = 0; i1 < range;i1++){
-    count[i1] = 0;
+  for(i1 = 0; i1 < 100; i1++){
+    if(numbers[i1] != 0){
+      printf("%d ", numbers[i1]);
+    }
   }
 
-  printf("\nNow Enter the set of numbers you wish to sort\n: "); 
+  printf("\n\n");
+
+  sort(offset, first, last, numbers);
+  
+
+  
   
   
 }
-    
-  
-
-
-
-  
