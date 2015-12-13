9fddf3f3a0a13eca4e3d99439e2f3ef35a6e315e
U-RONALD\Ronald <ronaldbalchand@gmail.com>
2015-12-13 16:40:11 -0500

    hw 6 unfinished

--- C/CSE130/HWs/hw6a.c
+++ C/CSE130/HWs/hw6a.c
@@ -6,6 +6,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct{//creation of the struct for each student and their components
   char *name;
@@ -20,6 +21,7 @@ typedef struct{//creation of the struct for each student and their components
 
 
 //this function calcualates a students' final grade taking their final grade, midterm grade, an array of their assignments, and the number of assignments
+
 
 double finalCalculator(int midterm, int final, int *assignments, int numA){
   double ans;
@@ -50,17 +52,140 @@ double finalCalculator(int midterm, int final, int *assignments, int numA){
 
 
 int main(null){
+
   int numAssignments;
   int maxStudents = 200;
+  int numStudents = 0;
   student *students;
   char *filename;
   int currentStudent = 0;
+  FILE *filePointer;
 
+  
 
   printf("Enter Input Filename: ");
   scanf("%s", &filename);
   printf("Enter Number of Assignmment Grades: ");
   scanf(" %d", &numAssignments);
+  
+  printf("File name: %s", &filename); 
+  
+  filePointer = fopen(&filename, "r");
+
+  char line[128];
+
+
+  while(fgets(line, sizeof(line), filePointer)!= NULL){
+
+    int currentChar = 0;
+    
+    char name[25];
+    //get name from the line    
+    int spaces = 0;
+    int i1 = 0;
+    while(spaces < 2){
+      if(line[i1] == ' '){
+	spaces++;
+      }
+      i1++;
+    }
+    strncpy(name, line, i1);
+    name[i1] = '\0';
+    students[numStudents].name = name;
+    //name has been recorded
+
+    currentChar = i1;
+    while(line[currentChar] == ' '){
+      currentChar++;
+    }
+    char idArray[9];
+    strncpy(idArray, line+currentChar, 9);
+    idArray[9] = '\0';
+    char *ptr;
+    
+    int realID = strtol(idArray, &ptr, 10);
+    students[numStudents].id = realID; 
+    
+    currentChar+=9;
+    i1 = 0;
+
+    while(line[currentChar] == ' '){
+      currentChar++;
+    }
+
+    char midterm[4];
+    strncpy(midterm, line+currentChar, 4);
+    midterm[3] = '\0';
+    char *ptr2;
+
+    int realMidterm = strtol(midterm, &ptr2, 10);
+    students[numStudents].midterm = realMidterm;
+    currentChar+= 4;
+    
+    while(line[currentChar] == ' '){
+      currentChar++;
+    }
+
+    char final[4];
+    strncpy(final, line+currentChar, 4);
+    final[3] = '\0';
+    char *ptr3;
+
+    int realFinal = strtol(final, &ptr3, 10);
+    students[numStudents].final = realFinal;
+
+    currentChar += 4;
+    
+
+    while(line[currentChar] == ' '){
+      currentChar++;
+    }
+    int i2;
+
+    for(i2 = 0; i2 < numAssignments; i2++){
+      char temp[4];
+      strncpy(temp, line+currentChar, 4);
+      temp[3] = '\0';
+      char *temptr;
+
+      int grade = strtol(temp, &temptr, 10);
+      students[numStudents].assignments[i2] = grade;
+      currentChar++;
+      while(line[currentChar] == ' '){
+	currentChar++;
+      }
+    }
+
+
+    //double finalCalculator(int midterm, int final, int *assignments, int numA){
+      
+    students[numStudents].classGrade = finalCalculator(students[numStudents].midterm, students[numStudents].final, students[numStudents].assignments, numAssignments);
+    
+
+    
+  
+
+    
+
+
+
+
+
+
+
+
+
+
+
+    
+    printf("\n%d\n",students[numStudents].assignments[3]);
+
+    
+    
+			
+    
+    numStudents++;
+  }
   
   
   
